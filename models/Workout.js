3261c1b52b28208a8180daceed8dda41fa408dd0
Patrick Ceriale <patceriale@gmail.com>
2020-10-31 15:49:49 -0700

    updated schema

--- models/Workout.js
+++ models/Workout.js
@@ -1,24 +1,68 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    unique: true,
-    validate: [({ length }) => length >= 1, "Password should be longer."],
-  },
-  reps: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Rep",
-    },
-  ],
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+// const WorkoutSchema = new Schema({
+//   name: {
+//     type: String,
+//     unique: true,
+//     validate: [({ length }) => length >= 1, "Password should be longer."],
+//   },
+//   reps: [
+//     {
+//       type: Schema.Types.ObjectId,
+//       ref: "Rep",
+//     },
+//   ],
+//   date: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
 
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date(),
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name",
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes",
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true,
+    },
+  }
+);
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
